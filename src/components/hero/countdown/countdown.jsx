0fde85649f37ed77aa6426a5b7fedb4a81d88fb9
Sergi Díaz <161464330+soysergidiaz@users.noreply.github.com>
2026-02-07 10:57:20 +0100

    Contador funcionando correctamente

--- src/components/hero/countdown/countdown.jsx
+++ src/components/hero/countdown/countdown.jsx
@@ -1,10 +1,36 @@
-import TimeCounter from "./time-counter"
-import React, { useState, useEffect } from 'react';
+import TimeCounter from "./time-counter";
+import { useState, useEffect } from 'react';
 
 export default function Countdown () {
 
     const fechaBoda = new Date("2026-09-25T17:30").getTime();
-    const ahora = Date.now();
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+    function calculateTimeLeft() {
+        const difference = fechaBoda - +new Date();
+        let timeLeft = {};
+
+        if (difference > 0) {
+            timeLeft = {
+                dias: Math.floor(difference / (1000 * 60 * 60 * 24)),
+                horas: Math.floor((difference / (1000 * 60 * 60)) % 24),
+                minutos: Math.floor((difference / 1000 / 60) % 60),
+                segundos: Math.floor((difference / 1000) % 60)
+            }
+        }
+
+        return timeLeft;
+    }
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    })
+
+    console.log(timeLeft);
 
 
 
@@ -12,10 +38,10 @@ export default function Countdown () {
         <div className="countdown">
             <h2>Cuenta atrás para el gran día</h2>
             <div className="counter">
-                <TimeCounter reloj="Días" tiempo="00" />
-                <TimeCounter reloj="Horas" tiempo="00" />
-                <TimeCounter reloj="Minutos" tiempo="00" />
-                <TimeCounter reloj="Segundos" tiempo="00" />
+                <TimeCounter reloj="Días" tiempo={timeLeft.dias} />
+                <TimeCounter reloj="Horas" tiempo={timeLeft.horas} />
+                <TimeCounter reloj="Minutos" tiempo={timeLeft.minutos} />
+                <TimeCounter reloj="Segundos" tiempo={timeLeft.segundos} />
             </div>
         </div>
     )
